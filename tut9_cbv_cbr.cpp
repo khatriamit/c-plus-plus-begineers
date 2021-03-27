f495d1206dcdad53d3eb41a706e16cb0d4477019
pydevnepal <rajukhatri054@gmail.com>
2021-03-27 10:10:09 +0545

    call by value and call by reference example

--- tut9_cbv_cbr.cpp
+++ tut9_cbv_cbr.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
+//This is the example of call by reference using pointer.
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main()
 {
+    int a = 5, b = 6;
+    cout << "The value of a is: " << a << endl;
+    cout << "The value of b is: " << b << endl;
 
+    cout << "The values after swapping." << endl;
+    swap(&a, &b);
+    cout << "The value of a is: " << a << endl;
+    cout << "The value of b is: " << b << endl;
     return 0;
 }
